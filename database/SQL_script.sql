d8797d8d683b12e99ba93b5dccba20c5192f61a5
krzpiszcz <kpiszcz@yahoo.com>
2023-07-27 09:35:43 +0200

    SQL script change

--- database/SQL_script.sql
+++ database/SQL_script.sql
@@ -7,7 +7,15 @@
 {%- set survey = var('email_msl') -%}
 {%- set stg_det_rep = 'test_det_rep'-%}
 
+{%- set surveyname  = get_data_filtered(pTable,pColumns,surveys_filter)[0][0] -%}
+{%- set countries = get_data_filtered(pTable,pColumns,countries_filter) -%}
+
+
 select
      *
     ,to_timestamp_ntz(sysdate()::timestamp) RUN_TIMESTAMP
 from {{ ref(stg_det_rep) }}
+
+UNION ALL
+
+select * from tabletest
