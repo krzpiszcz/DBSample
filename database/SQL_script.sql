cb8fcf993a1a21619eb1570b9016349862a4fb2c
krzpiszcz <kpiszcz@yahoo.com>
2023-07-27 09:34:40 +0200

    SQL script change1

--- database/SQL_script.sql
+++ database/SQL_script.sql
@@ -5,9 +5,13 @@
     )
 }}
 {%- set survey = var('email_msl') -%}
+{# --- Config Parameters (PARAMETERS) #}
+{%- set pTable      = 'dwh_parameters' -%}
+{%- set pColumns    = ' PARAM_VALUE_INT, PARAM_VALUE_CHAR ' -%}
 {%- set stg_det_rep = 'test_det_rep'-%}
 
 select
      *
     ,to_timestamp_ntz(sysdate()::timestamp) RUN_TIMESTAMP
+    ,newfield1
 from {{ ref(stg_det_rep) }}
